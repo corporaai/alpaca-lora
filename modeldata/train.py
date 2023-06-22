b0d362e69b6dcf5539b1ed542f65bbaed1fb9511
navdeep dhakar <navdepdhakar1@gmail.com>
2023-06-22 17:45:56 +0530

    added model upload

--- modeldata/train.py
+++ modeldata/train.py
@@ -3,42 +3,24 @@ import requests
 import fire
 import subprocess
 import json
-import tqdm
-def upload_folder(url, folder_path, json_data):
-    # Create a session to persist the connection
-    session = requests.Session()
-
-    # Create a multi-part form-data request for files
-    files = []
-    print('uploading model data...')
-    print("files to be uploaded")
-
-    # Get the total number of files in the folder
-    total_files = len([file_name for file_name in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, file_name))])
-
+def upload_folder(url, folder_path):
     # Iterate over the files in the folder
-    for file_name in (os.listdir(folder_path)):
+    for file_name in os.listdir(folder_path):
         file_path = os.path.join(folder_path, file_name)
+
         # Check if the current item is a file
         if os.path.isfile(file_path):
-            # Append the file to the 'files' list
-            print(file_name)
-            files.append(('file', (file_name, open(file_path, 'rb'))))
+            # Open the file in binary mode
+            with open(file_path, 'rb') as file:
+                # Create the multipart form-data request
+                files = {'file': (file_name, file)}
+                response = requests.post(url, files=files)
 
-    # Send the multi-part form-data request with the progress bar
-    with tqdm(total=total_files, unit='file') as progress_bar:
-        response = session.post(url, files=files, data=json_data, stream=True)
-
-        for chunk in response.iter_content(chunk_size=1024):
-            if chunk:
-                # Update the progress bar based on the received data chunks
-                progress_bar.update(1)
-
-    # Check the response
-    if response.status_code == 200:
-        print("Upload successful, now you can deploy the model for ineference.")
-    else:
-        print("Upload failed.")
+                # Check the response
+                if response.status_code == 200:
+                    print(f"Uploaded {file_name} successfully.")
+                else:
+                    print(f"Failed to upload {file_name}.")
 
 def download_file(url, local_filename):
     # NOTE the stream=True parameter below
@@ -53,13 +35,7 @@ def download_file(url, local_filename):
                 f.write(chunk)
     print("download finished")
     return local_filename
-payload = {
-    "secretkey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiclVna3l1aUVVV1lUejhpbGVIU0FyeHR4ZWtDMyIsImlhdCI6MTY4NzQwODE1OX0.NJFe6OafIKb55nS0JeKxEXYNTc442FXLm2uIpdTQPB8",
-    "modelname": "alpaca-lora",
-    "modelrepo": "somerepo",
-    "modelbaseweights": "decapoda/llama",
-    "trainingtype": "lora"
-}
+
 def train_model( 
     model_name:str="",
     user_id:str=None,
@@ -78,7 +54,7 @@ def train_model(
             
         train(base_model="decapoda-research/llama-7b-hf", data_path='dataset.json', output_dir=f"{output_dir}", val_set_size=val_set_size)
         # print("uploading finetuned model to storage")
-        # upload_folder("http://localhost:8080/upload-model", './modeldata', payload)
+        # res = uplaod(url, output_dir)
         # print(res)
     else:
         print("please provide a valid user token")
--- uptest.py
+++ uptest.py
@@ -1,9 +1,8 @@
-from finetune import train
 import requests
-import fire
-import subprocess
+import os
 import json
-import tqdm
+from tqdm import tqdm
+
 def upload_folder(url, folder_path, json_data):
     # Create a session to persist the connection
     session = requests.Session()
@@ -36,23 +35,10 @@ def upload_folder(url, folder_path, json_data):
 
     # Check the response
     if response.status_code == 200:
-        print("Upload successful, now you can deploy the model for ineference.")
+        print("Upload successful.")
     else:
         print("Upload failed.")
 
-def download_file(url, local_filename):
-    # NOTE the stream=True parameter below
-    print("downloading train dataset...")
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        with open(local_filename, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192): 
-                # If you have chunk encoded response uncomment if
-                # and set chunk_size parameter to None.
-                #if chunk: 
-                f.write(chunk)
-    print("download finished")
-    return local_filename
 payload = {
     "secretkey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiclVna3l1aUVVV1lUejhpbGVIU0FyeHR4ZWtDMyIsImlhdCI6MTY4NzQwODE1OX0.NJFe6OafIKb55nS0JeKxEXYNTc442FXLm2uIpdTQPB8",
     "modelname": "alpaca-lora",
@@ -60,28 +46,5 @@ payload = {
     "modelbaseweights": "decapoda/llama",
     "trainingtype": "lora"
 }
-def train_model( 
-    model_name:str="",
-    user_id:str=None,
-    dataset_url:str=None
-    ):
-    if(user_id):
-        output_dir_base = model_name if model_name else "./alpaca-lora-finetuned"
-        output_dir = f"{output_dir_base}{user_id}"
-        download_file(dataset_url, "dataset.json")
-        val_set_size = 50
 
-        with open('dataset.json', 'r') as openfile:
-            # Reading from json file
-            json_object = json.load(openfile)
-            val_set_size = int(len(json_object)*0.1)
-            
-        train(base_model="decapoda-research/llama-7b-hf", data_path='dataset.json', output_dir=f"{output_dir}", val_set_size=val_set_size)
-        # print("uploading finetuned model to storage")
-        # upload_folder("http://localhost:8080/upload-model", './modeldata', payload)
-        # print(res)
-    else:
-        print("please provide a valid user token")
-
-if __name__ == "__main__":
-    fire.Fire(train_model)
+upload_folder("http://localhost:8080/upload-model", './modeldata', payload)
