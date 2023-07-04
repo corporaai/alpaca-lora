b9f261991b4fc060a2e63268e7f5e530f9ac2819
navdeep dhakar <navdepdhakar1@gmail.com>
2023-07-04 11:07:24 +0530

    added model conf in deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 
+python3 -m venv "finetune-alpaca"
+
+source "finetune-alpaca/bin/activate" 
+echo "venv activated"
+
 pip install -r requirements.txt
 
 cp finetune-alpaca/lib/python3.8/site-packages/bitsandbytes/libbitsandbytes_cuda117.so finetune-alpaca/lib/python3.8/site-packages/bitsandbytes/libbitsandbytes_cpu.so
