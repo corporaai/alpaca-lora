9c80424b36b4d7f652302ffe4a59f60f15b963a6
navdeep dhakar <navdepdhakar1@gmail.com>
2023-07-04 11:04:29 +0530

    added model conf in deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 
+pip install -r requirements.txt
+
+cp finetune-alpaca/lib/python3.8/site-packages/bitsandbytes/libbitsandbytes_cuda117.so finetune-alpaca/lib/python3.8/site-packages/bitsandbytes/libbitsandbytes_cpu.so
+echo "bnb cuda library modified"
+
 python generate.py \
     --load_8bit \
     --base_model 'decapoda-research/llama-7b-hf' \
